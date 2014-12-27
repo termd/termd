e2d393a0814e1089e8a1907e28a27fdb038b2545
Julien Viet <julien@julienviet.com>
2014-12-27 19:23:42 +0100

    Minor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
   public static void main(String[] args) throws Exception {
 
+/*
     StringBuffer l = new StringBuffer();
     for (int i = 0;i < 200;i++) {
       l.append("*");
@@ -21,7 +22,41 @@ public class Main {
 //    System.out.print("\033[100C");
 
     System.in.read();
+*/
 
+/*
+    for (int i = 0;i < 200;i++) {
+      System.out.print('*');
+    }
+    for (int i = 0;i < 100;i++) {
+      System.out.print('\b');
+    }
+    System.out.print("\u001B[K");
+      System.in.read();
+*/
+
+/*
+    System.out.println();
+    System.out.println("pc28:~ julien$ abc\"");
+    System.out.print("def\"");
+    Thread.sleep(2000);
+    System.out.print("\u001B[A");
+    Thread.sleep(2000);
+    System.out.print(":~ julien$ ");
+    Thread.sleep(2000);
+    System.out.print("\u001B[K");
+    Thread.sleep(2000);
+    System.out.print("\r\n");
+    Thread.sleep(2000);
+    System.out.print("\r");
+    Thread.sleep(2000);
+    System.out.print("\u001B[K");
+    Thread.sleep(2000);
+    System.out.print("\u001B[A");
+    Thread.sleep(2000);
+    System.out.print("pc28:~ julien$ ");
+    System.in.read();
+*/
   }
 
 }
