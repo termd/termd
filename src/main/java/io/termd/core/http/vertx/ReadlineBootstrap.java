1c841ab54f864157be06511b5d94ed65ffe6cfed
Julien Viet <julien@julienviet.com>
2015-02-01 21:03:07 +0100

    Initial tty.js (not working) integration

---
+++ src/main/java/io/termd/core/http/vertx/ReadlineBootstrap.java
@@ -0,0 +1,82 @@
+package io.termd.core.http.vertx;
+
+import org.vertx.java.core.AsyncResult;
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.Vertx;
+import org.vertx.java.core.VertxFactory;
+import org.vertx.java.core.buffer.Buffer;
+import org.vertx.java.core.http.HttpServer;
+import org.vertx.java.core.http.HttpServerRequest;
+import org.vertx.java.core.http.HttpServerResponse;
+import org.vertx.java.core.http.impl.MimeMapping;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
+ */
+public class ReadlineBootstrap {
+
+  public static void main(String[] args) throws Exception {
+    final CountDownLatch latch = new CountDownLatch(1);
+    Vertx vertx = VertxFactory.newVertx();
+    final HttpServer server = vertx.createHttpServer();
+    server.requestHandler(new Handler<HttpServerRequest>() {
+      @Override
+      public void handle(HttpServerRequest req) {
+
+        // Not the best but does the job
+        String path = req.path();
+        if ("/".equals(path)) {
+          path = "/index.html";
+        }
+        URL res = ReadlineBootstrap.class.getResource("/io/termd/core/http" + path);
+        HttpServerResponse resp = req.response();
+        try {
+          if (res != null) {
+            InputStream in = res.openStream();
+            Buffer buf = new Buffer();
+            byte[] tmp = new byte[256];
+            resp = req.response();
+            for (int l = 0;l != -1;l = in.read(tmp)) {
+              buf.appendBytes(tmp, 0, l);
+            }
+            int li = path.lastIndexOf('.');
+            if (li != -1 && li != path.length() - 1) {
+              String ext = path.substring(li + 1, path.length());
+              String contentType = MimeMapping.getMimeTypeForExtension(ext);
+              if (contentType != null) {
+                resp.putHeader(org.vertx.java.core.http.HttpHeaders.CONTENT_TYPE, contentType);
+              }
+            }
+            resp.putHeader(org.vertx.java.core.http.HttpHeaders.CONTENT_LENGTH, String.valueOf(buf.length()));
+            resp.write(buf);
+          } else {
+            resp.setStatusCode(404);
+          }
+        } catch(Exception e) {
+          e.printStackTrace();
+        } finally {
+          resp.end();
+        }
+      }
+    });
+    server.listen(8080, new Handler<AsyncResult<HttpServer>>() {
+      @Override
+      public void handle(AsyncResult<HttpServer> event) {
+        if (event.succeeded()) {
+          System.out.println("Server started on " + 8080);
+        } else {
+          System.out.println("Could not start");
+          event.cause().printStackTrace();
+          latch.countDown();
+        }
+      }
+    });
+    latch.await();
+  }
+
+}
