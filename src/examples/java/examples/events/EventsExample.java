f15657849acebbf5776c7890defe1de7aa970b80
Julien Viet <julien@julienviet.com>
2015-09-29 22:28:38 +0200

    TTY event example

---
+++ src/examples/java/examples/events/EventsExample.java
@@ -0,0 +1,42 @@
+package examples.events;
+
+import io.termd.core.tty.TtyConnection;
+
+/**
+ * This example show how to handle TTY events.
+ */
+public class EventsExample {
+
+  public static void handle(TtyConnection conn) {
+
+    conn.setEventHandler((event, key) -> {
+      switch (event) {
+        case INTR:
+          conn.write("You did a Ctrl-C\n");
+          break;
+        case SUSP:
+          conn.write("You did a Ctrl-Z\n");
+          break;
+        case EOF:
+          conn.write("You did a Ctrl-D: closing\n");
+          conn.close();
+          break;
+      }
+    });
+
+    conn.setSizeHandler(size -> {
+      conn.write("You resized your terminal to " + size + "\n");
+    });
+
+    conn.setTermHandler(term -> {
+      conn.write("Your terminal is " + term + "\n");
+    });
+
+    conn.setStdinHandler(keys -> {
+      for (int key : keys) {
+        conn.write("You typed " + key + "\n");
+      }
+    });
+  }
+
+}
